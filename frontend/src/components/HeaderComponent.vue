82a33f89b432e15cc46850304368f4f282d92c3d
AntonMaslik <maslik.fortech@gmail.com>
2025-03-04 00:02:14 +0300

    feat: add history link

--- frontend/src/components/HeaderComponent.vue
+++ frontend/src/components/HeaderComponent.vue
@@ -1,6 +1,11 @@
 <template>
     <header>
-      <h1 class="header-title">LinkExt</h1>
+      <div class="header-logo">
+        <h1 class="header-title">LinkExt</h1>
+      </div>
+      <nav class="header-nav">
+        <router-link to="/history" class="header-link">History</router-link>
+      </nav>
     </header>
 </template>
 
