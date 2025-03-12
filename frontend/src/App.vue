a08699173df2b020377dbe7b28c4923d057594f9
AntonMaslik <maslik.fortech@gmail.com>
2025-03-12 03:21:08 +0300

    feat: add homepage

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HomePage from './components/pages/HomePage.vue';
+
+
+</script>
+
+<template>
+  <HomePage></HomePage>
+</template>
+
+<style scoped>
+  body {
+    background-color: #f9f9f9;
+  }
+</style>
---
+++ frontend/src/components/pages/HomePage.vue
@@ -0,0 +1,172 @@
+<template>
+  <n-layout style="min-height: 100vh; display: flex; flex-direction: column;">
+
+    <n-layout-header style="text-align: center;">
+      <div style="padding: 40px 20px 25px;">
+        <a href="#"
+          style="font: 900 49px asap,arial; color: #e12524; letter-spacing: -1px; text-shadow: 0 2px 2px #ddd; word-wrap: break-word; text-decoration: none;">LinkExt</a>
+      </div>
+    </n-layout-header>
+
+    <n-layout-content style="flex: 1; padding: 20px; display: flex; justify-content: center;">
+      <main>
+        <section class="urlbox">
+          <h1>Paste the URL to be shortened</h1>
+          <n-form :size="'large'">
+      <n-form-item>
+        <n-input style="text-align: start;"  placeholder="Enter the link here"/>
+        <n-button type="error" style="background-color: #e12524;">
+          Shorten URL
+        </n-button>
+      </n-form-item>
+    </n-form>
+    <p style="text-align: center;">LinkExt is a free tool to shorten URLs and generate short links
+      URL shortener allows to create a shortened link making it easy to share
+    </p>
+        </section>
+
+        <section class="content">
+          <h2>Simple and fast URL shortener!</h2>
+          <p>ShortURL allows to shorten long links from Instagram, Facebook, YouTube, Twitter, Linked In, WhatsApp, TikTok, blogs and sites. Just paste the long URL and click the Shorten URL button. On the next page, copy the shortened URL and share it on sites, chat and emails. After shortening the URL, check how many clicks it received.</p>
+
+          <h2>Shorten, share and track</h2>
+          <p>Your shortened URLs can be used in publications, documents, advertisements, blogs, forums, instant messages, and other locations. Track statistics for your business and projects by monitoring the number of hits from your URL with our click counter.</p>
+
+          <n-grid :x-gap="12" :y-gap="8" :cols="3">
+
+            <n-grid-item>
+      <div>
+        <h3>Easy</h3>
+        <n-icon size="80">
+        <LikeOutlined />
+      </n-icon>
+        <p>ShortURL is easy and fast, enter the long link to get your shortened link</p>
+      </div>
+    </n-grid-item>
+
+    <n-grid-item>
+      <div>
+        <h3>Shortened</h3>
+        <n-icon size="80">
+          <AddLinkFilled />
+        </n-icon>
+        <p>Use any link, no matter what size, ShortURL always shortens</p>
+      </div>
+    </n-grid-item>
+
+    <n-grid-item>
+      <div>
+        <h3>Secure</h3>
+        <n-icon size="80">
+          <WifiSecure />
+        </n-icon>
+        <p>It is fast and secure, our service has HTTPS protocol and data encryption</p>
+      </div>
+    </n-grid-item>
+
+    <n-grid-item>
+      <div >
+        <h3>Statistics</h3>
+        <n-icon size="80">
+          <AiStatus />
+        </n-icon>
+        <p>Check the number of clicks that your shortened URL received</p>
+      </div>
+    </n-grid-item>
+
+    <n-grid-item>
+      <div >
+        <h3>Reliable</h3>
+        <n-icon size="80">
+          <Viruses />
+        </n-icon>
+        <p>All links that try to disseminate spam, viruses and malware are deleted</p>
+      </div>
+    </n-grid-item>
+
+    <n-grid-item>
+      <div>
+        <h3>Devices</h3>
+        <n-icon size="80">
+          <DeviceDesktop />
+        </n-icon>
+        <p>Compatible with smartphones, tablets and desktop</p>
+      </div>
+    </n-grid-item>
+
+          </n-grid>
+        </section>
+      </main>
+    </n-layout-content>
+
+    <n-layout-footer style="background-color: #f5f5f5; padding: 20px; background: #333; border-top: 4px solid #e12524;">
+      <div style="display: flex; justify-content: center; align-items: center;">
+        <span style="color: #fff">© 2025 LinkExt. All Rights Reserved.</span>
+      </div>
+
+      <div style="display: flex; justify-content: center; align-items: center; margin-top: 20px; padding: 20px;">
+        <!-- Footer Menu -->
+        <div style="display: inline-block; gap:20px;" class="footer-menu">
+          <n-button class="footer-link" style="margin-right: 20px; color: #e12524" text>Privacy
+            Policy</n-button>
+
+          <n-button class="footer-link" style="margin-right: 20px; color: #e12524" text>Terms of
+            Service</n-button>
+          <n-button class="footer-link" style="color: #e12524;" text>Help</n-button>
+        </div>
+      </div>
+
+    </n-layout-footer>
+  </n-layout>
+</template>
+
+<script setup>
+import { NLayout, NLayoutContent, NLayoutFooter, NForm, NFormItem, NButton, NInput, NGrid, NGridItem, NIcon} from 'naive-ui';
+import { LikeOutlined } from '@vicons/antd';
+import { AddLinkFilled } from '@vicons/material';
+import { WifiSecure, AiStatus } from '@vicons/carbon';
+import { Viruses } from '@vicons/fa';
+import { DeviceDesktop } from '@vicons/tabler';
+</script>
+
+<style scoped>
+.footer-link:hover {
+  text-decoration: underline;
+}
+
+.light-green {
+  height: 108px;
+  background-color: rgba(0, 128, 0, 0.12);
+}
+.green {
+  height: 108px;
+  background-color: rgba(0, 128, 0, 0.24);
+}
+
+.urlbox {
+    margin: 0 auto 20px auto;
+    max-width: 758px;
+    box-shadow: 0 1px 4px #ccc;
+    border-radius: 6px;
+    padding: 10px 30px 5px;
+    background: #fff;
+    text-align: center;
+}
+
+.content {
+  text-align: center;
+}
+
+@media (max-width: 1000px) {
+  .footer-menu {
+    width: 100%;
+    max-height: 24px;
+    overflow: hidden;
+    margin: 8px 0;
+    max-height: 100%;
+    padding: 0 7px;
+    text-align: center;
+    ;
+  }
+}
+</style>
