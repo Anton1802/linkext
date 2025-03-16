8adbac968b766977f7b6340ebe3ca014a21b21cb
AntonMaslik <maslik.fortech@gmail.com>
2025-03-16 21:10:09 +0300

    feat: build docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,4 +15,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  preview: {
+    port: 5173,
+    strictPort: true,
+   },
+   server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:5173",
+   },
 })
