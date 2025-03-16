7cd93e23cb4a1e4caf309907cf9e6ecc8631a59d
AntonMaslik <maslik.fortech@gmail.com>
2025-03-16 03:12:56 +0300

    feat: cron for delete expired link

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,13 +3,20 @@ import { Link } from './schemas/link.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { nanoid } from 'nanoid';
+import { Cron } from '@nestjs/schedule';
 
 @Injectable()
 export class AppService {
   constructor(@InjectModel(Link.name) private linkModel: Model<Link>) {}
 
-  createLink(url: string) {
+  async createLink(url: string) {
     try {
+      const existLink = await this.linkModel.findOne({ original: url });
+
+      if (existLink) {
+        return existLink;
+      }
+
       const shortCode = nanoid(6);
 
       return this.linkModel.create({ shorten: shortCode, original: url });
@@ -26,5 +33,12 @@ export class AppService {
     }
 
     return originalUrl;
+  }
+
+  @Cron('0 0 * * *')
+  async deleteExpiredLinks() {
+    await this.linkModel.deleteMany({
+      createdAt: { $lt: new Date(Date.now() - 86400000) },
+    });
   }
 }
