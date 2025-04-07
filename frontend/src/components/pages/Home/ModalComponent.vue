7e6eef66ec3c39c903c26f15fbfa0f5b67573e35
Anton1802 <anton.sup1802@gmail.com>
2025-04-07 23:16:38 +0300

    feat: add truncate url in modal urlbox

--- frontend/src/components/pages/Home/ModalComponent.vue
+++ frontend/src/components/pages/Home/ModalComponent.vue
@@ -1,11 +1,12 @@
 <template>
     <n-modal v-model:show="store.showModal">
         <n-card style="width: 600px" title="Shorten URL" :bordered="false" size="huge" role="dialog" aria-modal="true">
-            <p style="font-size: 18px; margin: 0; padding: 0;">Full URL: <a :href="store.originalUrl ? store.originalUrl : '#'">{{
-                    store.originalUrl }}</a></p>
+            <p style="font-size: 18px; margin: 0; padding: 0;">Full URL: <a
+                    :href="store.originalUrl ? store.originalUrl : '#'">{{ truncateUrl(store.originalUrl) }}</a></p>
             <n-form :size="'large'">
                 <n-form-item>
-                    <n-input style="text-align: start; margin-right: 2px;" :autofocus="true" readonly="true" placeholder="" status="error" :value="store.shortenLink"/>
+                    <n-input style="text-align: start; margin-right: 2px;" :autofocus="true" readonly="true"
+                        placeholder="" status="error" :value="store.shortenLink" />
                     <n-button type="error" :focusable="true" style="background-color: #e12524;" @click="copyLink">
                         Copy Link
                     </n-button>
@@ -33,20 +34,27 @@ export default defineComponent({
     setup() {
         const store = useSharedStore()
         const notification = useNotification();
-        const copyLink = async () => {
-        await navigator.clipboard.writeText(store.shortenLink ?? '');
 
-      notification["success"]({
-        content: 'Success',
-        meta: "Link Copied Successfully",
-        keepAliveOnHover: true,
-        duration: 2500
-      })
+        const truncateUrl = (url: string, maxLength = 40) => {
+            if (url.length <= maxLength) return url;
+            return `${url.substring(0, maxLength)}...`;
+        };
+
+        const copyLink = async () => {
+            await navigator.clipboard.writeText(store.shortenLink ?? '');
+
+            notification["success"]({
+                content: 'Success',
+                meta: "Link Copied Successfully",
+                keepAliveOnHover: true,
+                duration: 2500
+            })
         }
 
         return {
             copyLink,
             store,
+            truncateUrl
         }
     }
 })
